171c3d51bb1be22d6117fc60db515eb6521d086f
Jeevan Bastola <jeevanbastola986@gmail.com>
2022-01-16 10:21:30 -0500

    download data added

---
+++ navigation/Download_Favourite_Data/downloadedbookdata.js
@@ -0,0 +1,30 @@
+export const downloads=[
+    {
+        "id": 7274,
+        "name": "Cuerpo y mente de montaña"
+        },
+        {
+        "id": 7161,
+        "name": "El Agua de la Vida"
+        },
+        {
+        "id": 7164,
+        "name": "La moneda de Carlos"
+        },
+        {
+        "id": 7275,
+        "name": "¿Animales que pueden contar?"
+        },
+        {
+        "id": 7162,
+        "name": "Los dos hermanos"
+        },
+        {
+        "id": 7175,
+        "name": "Apostar hasta que no quede nada: la ludopatía"
+        },
+        {
+        "id": 7168,
+        "name": "Palenque: ¡Juguemos a la pelota!"
+        }
+]
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity, TextInput, ActivityIndicator,Image} from 'react-native';
 import { SafeAreaView,ScrollView } from 'react-native-safe-area-context';
 import { SearchBar } from 'react-native-screens';
-import rows from '../constants/homescreenrows'
+import {row1,row2,row3} from '../constants/homescreenrows';
 
 const booksURL="https://www.commonlit.org/api/v1/raw_content/lesson_templates?token=9759826c246d687a67328c4c81811bb108821e41af3c42ce41e6ff28ebf8bbba9737947232dd64e32df99a54a0add95c498001d917ca96258d50444af256a2dc";
 
@@ -12,6 +12,8 @@ export default function HomeScreen({ navigation }){
     const [data, setData] = useState([]);
     const [filteredData, setFilteredData]= useState([]);
     const [search, setSearch]= useState('');
+    const [homedata, sethomedata]=useState([]);
+
     useEffect(() => {
         fetchBooks();
         return()=>{
@@ -91,7 +93,7 @@ export default function HomeScreen({ navigation }){
                     keyExtractor = {({id}, index) => "#"+ id }
                     renderItem = {({ item }) => (
                         <View style = {{width: '48%', alignItems:"center", margin: '1%', borderWidth:'0.75', padding: 15}}>
-                            <TouchableOpacity onPress={() => {navigation.push("Detalles" , {book : item}); console.log(rows)}}>
+                            <TouchableOpacity onPress={() => {navigation.push("Detalles" , {book : item}); console.log("new log"); console.log(row1)}}>
                                 <Image source={{uri: 'https://reactjs.org/logo-og.png'}} style={{width: 180, height: 250}}/>
                                 <Text numberOfLines={1} style = {{textAlign: 'center', fontWeight: 'bold'}}>{item.name} </Text>
                             </TouchableOpacity>  
